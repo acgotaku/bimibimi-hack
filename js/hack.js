27bda22257f9e39cf1aa96ed07ccea3217c134cf
acgotaku <acgotaku311@gmail.com>
2022-08-08 20:01:17 +0900

    hack lark

--- js/hack.js
+++ js/hack.js
@@ -1,8 +1,23 @@
-const hackImage=function(){
-  console.log('hack image');
-  window.Image = function() {};
-  Object.defineProperty(window, "Image", { writable: false} );
+const hackLark=function(){
+  console.log('hack lark');
+  MutationObserver = (function() {
+    function MutationObserver(callBack) {
+      this.callBack = callBack;
+    }
+
+    MutationObserver.prototype.observe = function(element, options) {
+      console.log(element, options)
+    };
+
+    MutationObserver.prototype.disconnect = function() {
+    };
+
+    return MutationObserver;
+
+  })();
+  window.MutationObserver = MutationObserver;
+  console.log(window.MutationObserver);
 };
 const script = document.createElement("script");
-script.appendChild(document.createTextNode("(" + hackImage + ")();"));
+script.appendChild(document.createTextNode("(" + hackLark + ")();"));
 document.documentElement.appendChild(script);
